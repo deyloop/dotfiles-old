dd9368f431f7d060c6517cc799b68abbee816dac
Anurup Dey <anu.rup.dey98@gmail.com>
2020-10-07 08:44:26 +0530

    Adds auto save on changing buffer to emacs

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -89,3 +89,18 @@
         "c b" #'dap-breakpoint-toggle))
 
 (load! "colorful-points.el")
+
+;; automatically save buffers associated with files on buffer switch
+;; and on windows switch
+(defadvice switch-to-buffer (before save-buffer-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice other-window (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-up (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-down (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-left (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-right (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
