364422886d9419682e51025b593224196841f67f
Anurup Dey <anu.rup.dey98@gmail.com>
2020-10-12 19:48:09 +0530

    Updates doom config

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -5,10 +5,3 @@
  ;; If there is more than one, they won't work right.
  '(smtpmail-smtp-server "smtp.gmail.com")
  '(smtpmail-smtp-service 25))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:background "#121212"))))
- '(hl-line ((t (:background "#000000")))))
